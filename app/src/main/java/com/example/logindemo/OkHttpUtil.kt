75720e9e8f57aafbe84cd52bced6467cf8f93ec5
Sankar சங்கர் <sankar.curiosity@gmail.com>
2018-03-02 16:04:06 +0530

    enhance the login endpoint a little

---
+++ app/src/main/java/com/example/logindemo/OkHttpUtil.kt
@@ -0,0 +1,7 @@
+package com.example.logindemo
+
+/**
+ * Created by secret on 02/03/18.
+ */
+class OkHttpUtil {
+}
--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
-	"io/ioutil"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/kabukky/httpscerts"
 )
 
+type LoginReq struct {
+	Username string
+	Password string
+}
+
 func main() {
 
-	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		log.Println(ioutil.ReadAll(r.Body))
-		w.WriteHeader(http.StatusOK)
-	})
+	http.Handle("/login", handlers.LoggingHandler(os.Stderr, http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			x := LoginReq{}
+
+			decoder := json.NewDecoder(r.Body)
+			if err := decoder.Decode(&x); err != nil {
+				log.Println(err)
+				http.Error(w, "JSON decode failure: "+err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			log.Println(x)
+
+			w.WriteHeader(http.StatusOK)
+		})))
 
 	err := httpscerts.Generate("cert.pem", "key.pem", "127.0.0.1:8000")
 	if err != nil {
