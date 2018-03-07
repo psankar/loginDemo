6d57ce6275d61c8a2bd1f0cd79eb5b1893fb959b
Sankar சங்கர் <sankar.curiosity@gmail.com>
2018-03-07 15:02:39 +0530

    MyGroupsAdapter

--- app/src/main/java/com/example/logindemo/MyGroupsActivity.kt
+++ app/src/main/java/com/example/logindemo/MyGroupsActivity.kt
@@ -2,11 +2,16 @@ package com.example.logindemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_my_groups.*
 
 class MyGroupsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_groups)
+
+        recyclerView_mygroups.layoutManager = LinearLayoutManager(this)
+        recyclerView_mygroups.adapter = MyGroupsAdapter()
     }
 }
---
+++ app/src/main/java/com/example/logindemo/MyGroupsAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.logindemo
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by secret on 07/03/18.
+ */
+
+class MyGroupsAdapter : RecyclerView.Adapter<MyGroupsViewHolder>() {
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 2;
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MyGroupsViewHolder {
+        TODO("Not implemented yet")
+    }
+
+    override fun onBindViewHolder(holder: MyGroupsViewHolder?, position: Int) {
+        TODO("Not implemented yet")
+    }
+}
+
+class MyGroupsViewHolder(v: View) : RecyclerView.ViewHolder() {
+
+}
