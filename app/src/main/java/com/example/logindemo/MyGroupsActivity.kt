b03564479e74de11985083d95042dfb861b994ad
Sankar சங்கர் <sankar.curiosity@gmail.com>
2018-03-07 23:52:12 +0530

    Pass JWT from one activity to another

--- app/src/main/java/com/example/logindemo/MyGroupsActivity.kt
+++ app/src/main/java/com/example/logindemo/MyGroupsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.logindemo
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_my_groups.*
 
 class MyGroupsActivity : AppCompatActivity() {
@@ -12,6 +14,22 @@ class MyGroupsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_my_groups)
 
         recyclerView_mygroups.layoutManager = LinearLayoutManager(this)
-        recyclerView_mygroups.adapter = MyGroupsAdapter(arrayOf("Hello", "ClientApp"))
+        recyclerView_mygroups.adapter = MyGroupsAdapter(arrayOf())
+
+        val fetchTask = MyGroupsFetchTask()
+        fetchTask.execute(null as Void?)
+
+    }
+
+    inner class MyGroupsFetchTask internal constructor() : AsyncTask<Void, Void, Boolean>() {
+        override fun doInBackground(vararg params: Void?): Boolean {
+            Log.i("MyGroupsFetchTask", intent.getStringExtra("JWT"))
+            return true
+        }
+
+        override fun onPostExecute(result: Boolean?) {
+            recyclerView_mygroups.adapter = MyGroupsAdapter(arrayOf("Hello", "Kotlin"))
+        }
+
     }
 }
